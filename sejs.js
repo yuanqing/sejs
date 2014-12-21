779549a305f7ce0f1892cc23a5bd7a70165a1ee2
yuanqing <hello@yuanqing.sg>
2014-12-21 20:53:04 +0800

    Major API changes

---
+++ sejs.js
@@ -0,0 +1,90 @@
+/* jshint quotmark: false */
+(function(factory) {
+  // istanbul ignore if
+  if (typeof exports === 'undefined') {
+    this.sejs = factory;
+  } else {
+    module.exports = exports = factory;
+  }
+})(function(tmpl) {
+
+  tmpl = tmpl.replace('\r', '');
+
+  var lines = tmpl.split('\n');
+  var str = []; // accumulators as we iterate over each character in `tmpl`
+  var buf = [];
+  var compiled = []; // array of lines of the compiled function
+  var n = 1; // the current line number
+  var isNewLine = false; // true if we have iterated onto a new line
+  var isEcho = false; // true if we are between '<%=' and '%>'
+  var i = -1;
+  var len = tmpl.length;
+
+  var pushStrToBuf = function() {
+    if (str.length) {
+      str = str.join('');
+      if (isEcho) {
+        str = str.trim();
+        buf.push(isNewLine ? "(n=" + n + ",l='" + lines[n] + "'," + str + ")" : str);
+        isNewLine = false;
+      } else {
+        buf.push("'" + str + "'");
+      }
+      str = [];
+    }
+  };
+
+  var pushBufToCompiled = function() {
+    if (buf.length) {
+      compiled.push('b.push(' + buf.join(',') + ');');
+      buf = [];
+    }
+  };
+
+  while (++i < len) {
+    switch (tmpl[i] + tmpl[i+1]) {
+    case '<%':
+      pushStrToBuf();
+      if (tmpl[i+2] === '=') { // echo
+        isEcho = true;
+        i++;
+      }
+      i++;
+      break;
+    case '%>':
+      if (isEcho) {
+        pushStrToBuf();
+        isEcho = false;
+      } else {
+        pushBufToCompiled();
+        if (isNewLine) {
+          compiled.push("n=" + n + ";l='" + lines[n] + "';");
+          isNewLine = false;
+        }
+        compiled.push(str.join('').trim());
+        str = [];
+      }
+      i++;
+      break;
+    default:
+      switch (tmpl[i]) {
+      case '\n':
+        isNewLine = true;
+        str.push('\\n');
+        n++;
+        break;
+      case '\\':
+        str.push('\\\\');
+        break;
+      default:
+        str.push(tmpl[i]);
+        break;
+      }
+    }
+  }
+  pushStrToBuf();
+  pushBufToCompiled();
+
+  return new Function('d', "try{var n=1,b=[],l='" + lines[0] + "';with(d||{}){" + compiled.join('') + "}return b.join('');}catch(e){e.message=n+'\\n'+e.message;throw e;}");
+
+});
---
+++ test/sejs.spec.js
@@ -0,0 +1,36 @@
+/* globals describe, it, expect */
+'use strict';
+
+var sejs = require('..');
+
+describe('sejs(tmpl)(data)', function() {
+
+  it('works without `data`', function() {
+    expect(sejs('foo')()).toBe('foo');
+  });
+
+  it('should work', function() {
+    expect(sejs('')({ foo: 'bar' })).toBe('');
+    expect(sejs('<p><%= foo %></p>')({ foo: 'bar' })).toBe('<p>bar</p>');
+    expect(sejs('<% if (foo) { %><p>\n<%= foo %>\n</p><% } %>')({ foo: 'bar' })).toBe('<p>\nbar\n</p>');
+  });
+
+  it('allows \\\\ and \\n in `tmpl`', function() {
+    expect(sejs('<%= foo %>\\baz')({ foo: 'bar' })).toBe('bar\\baz');
+    expect(sejs('<%= foo %>\nbaz')({ foo: 'bar' })).toBe('bar\nbaz');
+  });
+
+  it('strips \\r from `tmpl`', function() {
+    expect(sejs('<%= foo %>\rbaz')({ foo: 'bar' })).toBe('barbaz');
+  });
+
+  it('throws if a variable referenced in `tmpl` is not in `data`', function(done) {
+    try {
+      sejs('<%= foo %>')({});
+    } catch(err) {
+      expect(err.message.indexOf('foo is not defined') !== -1).toBe(true);
+      done();
+    }
+  });
+
+});
